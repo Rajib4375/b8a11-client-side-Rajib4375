65d256d85ebec44929cfedaeb096d4fe34a201e3
rajib4375 <rajibhossain10498@gmail.com>
2023-11-08 01:57:40 +0600

    added firebase setting and sing up form

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
+import Authprovider from './Provider/Authprovider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='container mx-auto'>
     <React.StrictMode>
+    <Authprovider>
     <RouterProvider router={router} />
+    </Authprovider>
   </React.StrictMode>,
   </div>
 )
